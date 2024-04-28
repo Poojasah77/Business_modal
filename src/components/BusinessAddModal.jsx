130c5f90723bbe986b84da9830a4388bec472c43
poojasah77 <poojasah77@gmail.com>
2024-04-28 18:31:03 +0545

    dashboard changed

--- src/components/BusinessAddModal.jsx
+++ src/components/BusinessAddModal.jsx
@@ -1,77 +1,179 @@
-import React from 'react'
-import { Map } from "./Map";
-import { GiTireIronCross } from "react-icons/gi";
-import { useState } from "react"
+import { useState } from "react";
+import Map from './Map';
+import { RxCross2 } from 'react-icons/rx';
 
-const BusinessAddModal = ({modalIsOpen, setModalIsOpen, handleAddBusiness}) => {
+const BusinessAddModal = ({ businessAddModalIsOpen, setBusinessAddModalIsOpen, handleAddBusiness }) => {
     const [businessName, setBusinessName] = useState("");
     const [businessDate, setBusinessDate] = useState("");
-    const [currentMarkerPosition, setCurrentMarkerPosition] = useState({
-        lat:51.505 ,
-        lng:-0.09
+    const [markerCoords, setMarkerCoords] = useState({
+        lat: 27.678762,
+        lng: 85.349538,
     });
 
-    const handleNameChange = (e) => { 
-      setBusinessName(e.target.value)
+    const handleBusinessNameChange = (e) => {
+        setBusinessName(e.target.value);
     }
-    const handleDateChange = (e) => {
-      setBusinessDate(e.target.value)
-    }
-  
 
-    const closeModal = () => setModalIsOpen(false);
+    const handleBusinessDateChange = (e) => {
+        setBusinessDate(e.target.value);
+    }
+
+    const onClose = () => {
+        setBusinessAddModalIsOpen(false);
+    }
 
     const handleSubmit = () => {
-      handleAddBusiness({
-        name: businessName,
-        createdOn: businessDate,
-        location: currentMarkerPosition
-      });
-
-      setBusinessName("");
-      setBusinessDate("");
-      closeModal();
+        handleAddBusiness({
+            id: parseInt(Math.random() * 1000000),
+            name: businessName,
+            createdOn: businessDate,
+            location: markerCoords,
+        });
+        onClose();
     }
 
+    return (
+        <div
+            onClick={onClose}
+            className={`
+              inset-0 
+              fixed 
+              h-screen 
+              w-screen 
+              flex justify-center items-center 
+              transition-all 
+              ${businessAddModalIsOpen ? 'bg-black/60 visible' : 'invisible'}
+            `}>
+            <div
+                onClick={(e) => e.stopPropagation()}
+                className={`
+                  h-auto w-96
+                  bg-white 
+                  rounded-lg 
+                  shadow-lg 
+                  transition-all 
+                  ${businessAddModalIsOpen ? 'translate-y-0 scale-100' : 'translate-y-[900px] scale-0'} 
+                  px-6 py-8
+                `}>
+                <div className="flex justify-between items-center">
+                    <span className="font-bold text-3xl text-slate-800">Add a Business</span>
+                    <RxCross2 onClick={onClose} size={30} color='gray' className='cursor-pointer' />
+                </div>
 
-  return (
-    <>
-    <div onClick={closeModal} className={`inset-0 absolute h-screen w-screen flex justify-center items-center transition-all ${modalIsOpen? 'bg-black/60 visible ':"invisible"}`}>
-            <div onClick={(e => e.stopPropagation())} className={`h-[90%] w-1/3 bg-white rounded-lg py-4 px-8 shadow-lg ${modalIsOpen ? "translate-y-0": "translate-y-[900px]" } ${modalIsOpen ? "scale-100": "scale-0" }`}>
-              <div className="flex flex-row justify-between items-center">
-                <span className="text-lg font-bold">Add Business</span>
-                <GiTireIronCross  size={20} color="gray" onClick={closeModal} />
-              </div>
+                <div className="
+                  my-5 
+                  w-full 
+                  border-b-2 
+                  border-rose-500 
+                  rounded-full
+                ">
 
+                </div>
 
-              <div className="my-5 w-full border-rose-500 border-b-2 rounded-full"></div>
+                <span className="font-bold text-xl text-slate-800">Add Business Information</span>
+                <div className="flex flex-col py-4 relative justify-center">
+                    <input
+                        onChange={handleBusinessNameChange}
+                        value={businessName}
+                        type="text"
+                        placeholder=" "
+                        className="
+                          border-rose-500 
+                          border-[3px] 
+                          rounded-lg 
+                          p-3 
+                          outline-none 
+                          peer"
+                    />
+                    <span
+                        className="
+                            absolute left-3 
+                            text-gray-500 
+                            transition-all 
+                            flex 
+                            bg-white 
+                            peer-focus:-translate-y-6
+                            peer-placeholder-shown:-translate-y-0 
+                            -translate-y-6 
+                            pointer-events-none 
+                            px-1
+                            peer-focus:text-xs 
+                            peer-[:not(:placeholder-shown)]:text-xs 
+                            text-md
+                        ">
+                        Business Name
+                    </span>
+                </div>
 
-              <div className="font-bold text-lg text-slate-800">Add Business Information</div>
-              <div className="flex flex-col py-4 relative justify-center">
-                <input onChange={handleNameChange} value={businessName} type="text" placeholder=" " className="border-rose-500 border-[3px] rounded-lg p-3 outline-none peer" />
-                <span className="absolute left-3 text-gray-500 transition-all flex bg-white peer-focus:-translate-y-6  peer-focus:translate-x-30 peer-placeholder-shown:-translate-y-0 peer-placeholder-shown:translate-x-0 -translate-y-6 translate-x-30  pointer-events-none peer-focus:text-xs peer-[:not(:placeholder-shown)]:text-xs text-md">business name</span>
-              </div>
+                <div className="flex flex-col py-4 relative justify-center">
+                    <input
+                        onChange={handleBusinessDateChange}
+                        value={businessDate}
+                        type="text"
+                        placeholder=" "
+                        className="
+                          border-rose-500 
+                          border-[3px] 
+                          rounded-lg 
+                          p-3 
+                          outline-none 
+                          peer"
+                    />
+                    <span
+                        className="
+                            absolute left-3 
+                            text-gray-500 
+                            transition-all 
+                            flex 
+                            bg-white 
+                            peer-focus:-translate-y-6
+                            peer-placeholder-shown:-translate-y-0 
+                            -translate-y-6 
+                            pointer-events-none 
+                            px-1
+                            peer-focus:text-xs 
+                            peer-[:not(:placeholder-shown)]:text-xs 
+                            text-md
+                        "
+                    >Business Organization Date</span>
+                </div>
 
-              <div className="flex flex-col py-4 relative justify-center">
-                <input onChange={handleDateChange} value={businessDate} type="text" placeholder=" " className="border-rose-500 border-[3px] rounded-lg p-3 outline-none peer" />
-                <span className="absolute left-3 text-gray-500 transition-all flex bg-white peer-focus:-translate-y-6  peer-focus:translate-x-30 peer-placeholder-shown:-translate-y-0 peer-placeholder-shown:-translate-x-0 -translate-y-6 translate-x-30  pointer-events-none peer-focus:text-xs peer-[:not(:placeholder-shown)]:text-xs text-md">Organization Date</span>
-              </div>
+                <span className="font-bold text-xl text-slate-800">Add Business Location</span>
+                <div className="w-full h-1/2 mt-2 overflow-hidden">
+                    <Map currentMarkerPosition={markerCoords} setCurrentMarkerPosition={setMarkerCoords} />
+                </div>
 
-
-              <span className="font-bold text-xl text-slate-800">Add Business Location</span>
-              <div className="overflow-hidden w-full h-1/3 mt-2">
-                <Map currentMarkerPosition={currentMarkerPosition} setCurrentMarkerPosition={setCurrentMarkerPosition} />
-              </div>
-
-              <div className="flex flex-row justify-end gap-4 my-4">
-                <button  onClick={closeModal} className="text-rose-500 text-bold border border-rose-500 py-2 px-4 rounded-md hover:text-rose-500/60 hover:border-rose-500/60">Close</button>
-                <button onClick={handleSubmit} className="bg-rose-500 py-2 px-4 rounded-md text-white hover:bg-rose-500/80">Submit</button>
-              </div>
-               
+                <div className="flex justify-end items-center gap-4 my-4">
+                    <button
+                        onClick={onClose}
+                        className="
+                          px-5 
+                          py-2 
+                          border 
+                          border-rose-500 
+                          text-rose-500 
+                          rounded-md 
+                          font-bold 
+                          text-lg 
+                          hover:border-rose-500/90 
+                          hover:text-rose-500/60"
+                    >Cancel</button>
+                    <button
+                        onClick={handleSubmit}
+                        className="
+                          px-5 
+                          py-2 
+                          bg-rose-500 
+                          text-white 
+                          rounded-md 
+                          font-bold 
+                          text-lg 
+                          hover:bg-rose-500/90"
+                    >Submit</button>
+                </div>
             </div>
-          </div> 
-    </>
-  )
-}
+        </div>
+    );
+};
 
-export default BusinessAddModal
+export default BusinessAddModal;
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,26 +1,31 @@
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
-export const Map = ({currentMarkerPosition, setCurrentMarkerPosition}) => {
-    const position = [51.505, -0.09];
-    const LocationFinder = () => {
-      const map = useMapEvents({
-        click (e) { setCurrentMarkerPosition(e.latlng)} 
-      })
+const Map = ({ currentMarkerPosition, setCurrentMarkerPosition}) => {
+    
+    function LocationFinder() {
+        const map = useMapEvents({
+            click(e) {
+                setCurrentMarkerPosition && setCurrentMarkerPosition(e.latlng);
+            }
+        })
     }
-    return(
-        <MapContainer className="w-full h-full" center={[currentMarkerPosition.lat, currentMarkerPosition.lng]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[currentMarkerPosition.lat, currentMarkerPosition.lng]}>
-          {/* <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup> */}
-        </Marker>
-        <LocationFinder />
-      </MapContainer>
-    )
+
+    return (
+        <MapContainer className='w-full h-[150px]' center={[currentMarkerPosition.lat, currentMarkerPosition.lng]} zoom={17} scrollWheelZoom={false}>
+            <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[currentMarkerPosition.lat, currentMarkerPosition.lng]}>
+            {/* <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup> */}
+            </Marker>
+
+            <LocationFinder />
+        </MapContainer>
+    ) 
 }
 
+export default Map;
